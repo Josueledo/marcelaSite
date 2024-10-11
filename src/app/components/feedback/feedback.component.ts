e92d3eb63aada1bf621b65c12ed94f550d919ff7
josueledo <josueledo12@gmail.com>
2024-10-11 00:08:17 -0300

    Update: upgrade feedback

--- src/app/components/feedback/feedback.component.ts
+++ src/app/components/feedback/feedback.component.ts
@@ -6,11 +6,25 @@ import { Router } from '@angular/router';
   standalone: true,
   imports: [],
   templateUrl: './feedback.component.html',
-  styleUrl: './feedback.component.scss'
+  styleUrl: './feedback.component.scss',
 })
 export class FeedbackComponent {
-  route = inject(Router)
-  home(){
-    this.route.navigateByUrl("/home")
+  selecteds: any[] = [];
+  route = inject(Router);
+  home() {
+    this.route.navigateByUrl('/home');
+  }
+  select(e: any) {
+    if(this.selecteds.length > 0){
+      for (let i = 0; this.selecteds.length > i; i++) {
+        this.selecteds[i].style.color = 'rgb(133, 133, 133)';
+        this.selecteds[i].style.background = 'transparent';
+      }
+      this.selecteds = [];
+    }
+    this.selecteds.push(e);
+    console.log(this.selecteds);
+    this.selecteds[0].style.color = 'rgb(238, 167, 35)';
+    this.selecteds[0].style.background = ' rgb(233, 231, 229)';
   }
 }
